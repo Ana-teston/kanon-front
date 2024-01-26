5ab36500fd7cdb6d5a6585e3c6f71cf00b309b63
Ana Paula Teston <anapaulateston@imac-de-ana.localdomain>
2024-01-26 19:55:37 +0100

    return from endpoint the result of the spin and the coins the player won

--- src/components/slot-machine/slot-machine.component.tsx
+++ src/components/slot-machine/slot-machine.component.tsx
@@ -5,7 +5,7 @@ import {AppDispatch, RootState} from '../../state/store';
 
 const SlotMachinePage: React.FC = () => {
     const dispatch: AppDispatch = useDispatch();
-    const { spinResult, loading } = useSelector((state: RootState) => state.slotMachine);
+    const { spinResult, coinsWon, updatedCoins, loading } = useSelector((state: RootState) => state.slotMachine);
 
     const handleSpinClick = () => {
         dispatch(spinSlotMachine());
@@ -18,6 +18,8 @@ const SlotMachinePage: React.FC = () => {
                 {loading ? 'Spinning...' : 'Spin the Slot Machine'}
             </button>
             {spinResult && <p>Result: {spinResult.join(', ')}</p>}
+            {coinsWon !== null && <p>Coins Won: {coinsWon}</p>}
+            {updatedCoins !== null && <p>You have: {updatedCoins}, Try again!</p>}
         </div>
     );
 };
